8e0e7927e1267fddb388b83bb05390a85810635d
Jamie Pittman <jlp0328@gmail.com>
2020-03-31 10:17:57 -0400

    Finished tutorial
    How:
    ****

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+
 import logo from "./logo.svg";
 import "./App.css";
 
+import rootReducer from "./rootReducer";
+
 import MoviesList from "./MoviesList";
 import MovieDetail from "./MovieDetail";
+import Toggle from "./Toggle";
 
-const hello = () => "hello";
-const store = createStore(hello);
+const middleware = [logger, thunk];
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 const App = () => (
   <Provider store={store}>
@@ -21,6 +33,7 @@ const App = () => (
             <img src={logo} className="App-logo" alt="logo" />
           </Link>
         </header>
+        <Toggle />
         <Switch>
           <Route exact path="/" component={MoviesList} />
           <Route path="/:id" component={MovieDetail} />
